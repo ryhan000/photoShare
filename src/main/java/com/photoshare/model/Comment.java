0cae457b6825508ff72f6893e0b381326df4d4a8
ryhan000 <rayhan1ahamed@gmail.com>
2018-04-23 18:52:18 +0600

    added model

---
+++ src/main/java/com/photoshare/model/Comment.java
@@ -0,0 +1,63 @@
+package com.photoshare.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Comment {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long commetId;
+
+    private String coment;
+
+
+    @ManyToOne
+    @JsonIgnore
+    private Photo photo;
+
+    private Long photoId;
+    private String userName;
+    
+    
+	public Long getCommetId() {
+		return commetId;
+	}
+	public void setCommetId(Long commetId) {
+		this.commetId = commetId;
+	}
+	public String getComent() {
+		return coment;
+	}
+	public void setComent(String coment) {
+		this.coment = coment;
+	}
+	public Photo getPhoto() {
+		return photo;
+	}
+	public void setPhoto(Photo photo) {
+		this.photo = photo;
+	}
+	public Long getPhotoId() {
+		return photoId;
+	}
+	public void setPhotoId(Long photoId) {
+		this.photoId = photoId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+    
+    
+    
+    
+    
+}
